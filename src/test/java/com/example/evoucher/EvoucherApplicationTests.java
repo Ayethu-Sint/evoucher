e8a6cbf0bc3752e85bf518f3117ed9ebe6e086b3
Ayethu-Sint <ayethusint@gmail.com>
2025-01-18 07:56:50 +0800

    Initial Commit

---
+++ src/test/java/com/example/evoucher/EvoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evoucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoucherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
